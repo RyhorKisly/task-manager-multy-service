78bad0bed2e204d861fe60a853c19e49cfb2477a
Gregory <RyhorKisly@gmail.com>
2023-07-27 21:58:58 +0300

    add ddl for users

---
+++ postgresql/user-service/ddl/init_user_service.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE user_service
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+CREATE SCHEMA app
+    AUTHORIZATION postgres;
+
+CREATE TABLE app."users"
+(
+    uuid uuid NOT NULL,
+    dt_create timestamp without time zone NOT NULL,
+    dt_update timestamp without time zone NOT NULL,
+    mail text NOT NULL,
+    fio text NOT NULL,
+    role text NOT NULL,
+    status text NOT NULL,
+    password text NOT NULL,
+    PRIMARY KEY (uuid)
+);
