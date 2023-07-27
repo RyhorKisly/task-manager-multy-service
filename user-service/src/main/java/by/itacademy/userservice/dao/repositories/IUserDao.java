5adfc4b3c2bb280029016b16a1743cb01d509a47
Gregory <RyhorKisly@gmail.com>
2023-07-27 21:54:03 +0300

    add IUserDao

---
+++ user-service/src/main/java/by/itacademy/userservice/dao/repositories/IUserDao.java
@@ -0,0 +1,28 @@
+package by.itacademy.userservice.dao.repositories;
+
+import by.itacademy.userservice.dao.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+@Repository
+public interface IUserDao extends JpaRepository<UserEntity, UUID> {
+    @Override
+    Page<UserEntity> findAll(Pageable pageable);
+    @Override
+    Optional<UserEntity> findById(UUID uuid);
+
+    @Override
+    <S extends UserEntity> S save(S entity);
+
+}
