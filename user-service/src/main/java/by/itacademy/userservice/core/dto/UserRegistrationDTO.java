5b3a8617780eafd6011271c34e9156cddef61455
Gregory <RyhorKisly@gmail.com>
2023-07-27 21:53:05 +0300

    add DTOS

---
+++ user-service/src/main/java/by/itacademy/userservice/core/dto/UserRegistrationDTO.java
@@ -0,0 +1,44 @@
+package by.itacademy.userservice.core.dto;
+
+public class UserRegistrationDTO {
+    private String mail;
+    private String fio;
+    private String password;
+
+    public UserRegistrationDTO() {
+    }
+
+    public UserRegistrationDTO(
+            String mail,
+            String fio,
+            String password
+    ) {
+        this.mail = mail;
+        this.fio = fio;
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
