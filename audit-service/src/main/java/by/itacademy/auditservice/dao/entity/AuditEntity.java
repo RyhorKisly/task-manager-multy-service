6e73fa47f0bce99bea78ec15c70ed6789725a20e
Gregory <RyhorKisly@gmail.com>
2023-07-30 15:23:21 +0300

    add: dao and entity

---
+++ audit-service/src/main/java/by/itacademy/auditservice/dao/entity/AuditEntity.java
@@ -0,0 +1,92 @@
+package by.itacademy.auditservice.dao.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SourceType;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "audit", schema = "app")
+public class AuditEntity {
+    @Id
+    private UUID uuid;
+    @CreationTimestamp(source = SourceType.DB)
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "dt_create")
+    private LocalDateTime dtCreate;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_uuid", referencedColumnName = "uuid")
+    private UserEntity user;
+    private String text;
+    private String type;
+    private String id;
+
+    public AuditEntity() {
+    }
+
+    public AuditEntity(
+            UUID uuid,
+            LocalDateTime dtCreate,
+            UserEntity user,
+            String text,
+            String type,
+            String id
+    ) {
+        this.uuid = uuid;
+        this.dtCreate = dtCreate;
+        this.user = user;
+        this.text = text;
+        this.type = type;
+        this.id = id;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public LocalDateTime getDtCreate() {
+        return dtCreate;
+    }
+
+    public void setDtCreate(LocalDateTime dtCreate) {
+        this.dtCreate = dtCreate;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
