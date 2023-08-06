b3c286e038a5175540f60631e69f6d2b0cac8400
Gregory <RyhorKisly@gmail.com>
2023-08-07 01:08:34 +0300

    refactor: dao

--- user-service/src/main/java/by/itacademy/userservice/dao/repositories/IVerificationDao.java
+++ user-service/src/main/java/by/itacademy/userservice/dao/repositories/IVerificationDao.java
@@ -1,13 +1,15 @@
 package by.itacademy.userservice.dao.repositories;
 
-import by.itacademy.userservice.dao.entity.VerificationTokenEntity;
+import by.itacademy.userservice.dao.entity.VerificationEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface IVerificationDao extends CrudRepository<VerificationTokenEntity, UUID> {
-    Optional<VerificationTokenEntity> findByToken(UUID token);
+public interface IVerificationDao extends CrudRepository<VerificationEntity, UUID> {
+    Optional<VerificationEntity> findByMail(String mail);
+    void deleteByDtCreateLessThan(LocalDateTime dateTime);
 }
