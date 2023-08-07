9ee67be2ed821406e3dd72b33c5daaa06e67be64
Gregory <RyhorKisly@gmail.com>
2023-08-07 17:56:56 +0300

    add code for security and base interaction with user-service

---
+++ task-service/src/main/java/by/itacademy/taskservice/service/feign/UserServiceClient.java
@@ -0,0 +1,18 @@
+package by.itacademy.taskservice.service.feign;
+
+import by.itacademy.taskservice.core.dto.UserShortDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+//TODO переместить запись урла в файл с пропертями
+@FeignClient(value = "user-service", url = "http://localhost:8080/users/general")
+public interface UserServiceClient {
+
+    @GetMapping
+    ResponseEntity<UserShortDTO> send(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken
+            );
+}
